b32a6fe22856dc6e71c19982941bd8309a09d332
Dryehm <79123285+Dryehm@users.noreply.github.com>
2021-10-28 21:39:59 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 Events.on(ClientLoadEvent, b  => {
 Blocks.arc.targetAir = true;
 Bullets.damageLightningGround.collidesAir = true;
-Blocks.arc.shootType.lightningRange = 30
+Blocks.arc.shootType.lightningLength = 30
 Blocks.arc.range=130
 });
